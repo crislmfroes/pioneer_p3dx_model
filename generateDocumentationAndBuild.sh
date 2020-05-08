c686f55373966ea8f9ddb763668293957c8a347b
crislmfroes <cris.lima.froes@gmail.com>
2020-05-07 22:45:27 -0300

    now it finally should deploy all the docs

--- generateDocumentationAndBuild.sh
+++ generateDocumentationAndBuild.sh
@@ -12,18 +12,18 @@ git config user.name "Travis CI GIR"
 git config user.email "travis@travis-ci.org"
 rm -rf *
 echo "" > .nojekyll
-for package in ../../p3dx_*
+for path in ../../p3dx_*
 do
-    echo "Generating rosdoc_lite code documentation for package $package ..."
-    rosdoc_lite ../../$package -o $package 2>&1 | tee $package.log
-    if [ -d "$package/html" ] && [ -f "$package/html/index.html" ]; then
-        echo "Uploading $package package documentation to gh-pages branch ..."
+    echo "Generating rosdoc_lite code documentation for package $(basename $path) ..."
+    rosdoc_lite $path -o $(basename $path) 2>&1 | tee $(basename $path).log
+    if [ -d "$(basename $path)/html" ] && [ -f "$(basename $path)/html/index.html" ]; then
+        echo "Uploading $(basename $path) package documentation to gh-pages branch ..."
         git add --all
-        git commit -m "Deploy $package docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
+        git commit -m "Deploy $(basename $path) docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
     else
         echo '' >&2
-        echo "Warning: No documentation (html) files have been found for package $package !" >&2
-        echo "Warning: Not going to push the documentation to GitHub for packge $package !" >&2
+        echo "Warning: No documentation (html) files have been found for package $(basename $path) !" >&2
+        echo "Warning: Not going to push the documentation to GitHub for packge $(basename $path) !" >&2
         exit 1
     fi
 done
